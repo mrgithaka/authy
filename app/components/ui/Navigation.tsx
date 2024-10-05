a1853a56a420298d159d4ba3a11c6ab66c0084bc
mrgithaka <mrgithaka@gmail.com>
2024-10-05 15:23:47 +0300

    feat(ui): Navigation - added more Navigation component content with responsiveness in mind

--- app/components/ui/Navigation.tsx
+++ app/components/ui/Navigation.tsx
@@ -1,5 +1,17 @@
 import { NavigationStyles } from '@styles/styles';
 
 export default function Navigation({ children }: NavigationProps) {
-    return <nav className={NavigationStyles.navigation}>{children}</nav>;
+    return (
+        <nav className={NavigationStyles.navigation}>
+            <div className={NavigationStyles.navigation_large}>{children}</div>
+            <div className={NavigationStyles.navigation_small}>
+                <div
+                    className={NavigationStyles.navigation_small_buttons}
+                ></div>
+                <div className={NavigationStyles.navigation_small_menu}>
+                    {children}
+                </div>
+            </div>
+        </nav>
+    );
 }
