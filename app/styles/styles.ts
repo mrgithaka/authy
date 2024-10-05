a5017201fcd9b3888f9aa168d2eb435fd633b02b
mrgithaka <mrgithaka@gmail.com>
2024-10-05 13:33:59 +0300

    feat: styles registry - created a styles registry for all css modules

---
+++ app/styles/styles.ts
@@ -0,0 +1,3 @@
+export { default as HeaderStyles } from './layout/header.module.css';
+export { default as MainStyles } from './layout/main.module.css';
+export { default as FooterStyles } from './layout/footer.module.css';
