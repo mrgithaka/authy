064edfb1ff336cac7dd05d615b4eed63544316cf
mrgithaka <mrgithaka@gmail.com>
2024-10-05 15:37:53 +0300

    feat(ui): Navigation - added toggle menu logic

--- app/components/ui/Navigation.tsx
+++ app/components/ui/Navigation.tsx
@@ -1,13 +1,37 @@
+'use client';
+import { useState } from 'react';
 import { NavigationStyles } from '@styles/styles';
+import Image from 'next/image';
 
 export default function Navigation({ children }: NavigationProps) {
+    const [menu, setMenu] = useState(false);
+    function toggleMenu() {
+        return setMenu(!menu);
+    }
     return (
         <nav className={NavigationStyles.navigation}>
             <div className={NavigationStyles.navigation_large}>{children}</div>
-            <div className={NavigationStyles.navigation_small}>
-                <div
-                    className={NavigationStyles.navigation_small_buttons}
-                ></div>
+            <div
+                className={NavigationStyles.navigation_small}
+                onClick={toggleMenu}
+            >
+                <div className={NavigationStyles.navigation_small_buttons}>
+                    {menu ? (
+                        <Image
+                            src={'./icons/close.svg'}
+                            alt='Menu Icon'
+                            width={40}
+                            height={40}
+                        />
+                    ) : (
+                        <Image
+                            src={'./icons/menu.svg'}
+                            alt='Menu Icon'
+                            width={40}
+                            height={40}
+                        />
+                    )}
+                </div>
                 <div className={NavigationStyles.navigation_small_menu}>
                     {children}
                 </div>
