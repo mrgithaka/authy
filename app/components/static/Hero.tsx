f2b0021e4b9cc65a019777f603101725e1036024
mrgithaka <mrgithaka@gmail.com>
2024-10-05 17:01:17 +0300

    feat(static): Hero - added more content

--- app/components/static/Hero.tsx
+++ app/components/static/Hero.tsx
@@ -1,3 +1,27 @@
+import Image from 'next/image';
+import { NavigationItem } from '@components/components';
+
 export default function Hero() {
-    return <section></section>;
+    return (
+        <section>
+            <Image
+                src={'./logo'}
+                alt='Authy Logo'
+                width={50}
+                height={50}
+            />
+            <h1>Authy</h1>
+            <h2>Next.js Authentication</h2>
+            <div>
+                <NavigationItem
+                    name='Documentation'
+                    link=''
+                />
+                <NavigationItem
+                    name='Repository'
+                    link=''
+                />
+            </div>
+        </section>
+    );
 }
