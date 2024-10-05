34a275f2370c7abe563489617157cef29dbde6b6
mrgithaka <mrgithaka@gmail.com>
2024-10-05 11:28:06 +0300

    feat: metadata - added index/home/authy page metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Authy',
+    description:
+        'Authy is a small project designed to explore Next.js authentication and authorization.',
+};
+
 export default function Authy() {
     return <div></div>;
 }
