0241fdc887f02ed37a4ad2fee64519e440b55b3f
mrgithaka <mrgithaka@gmail.com>
2024-10-07 08:25:27 +0300

    feat(layout): Authentication - created Authentication component - registered the component

--- app/components/components.ts
+++ app/components/components.ts
@@ -1,10 +1,8 @@
 export { default as Header } from './layout/Header';
 export { default as Main } from './layout/Main';
 export { default as Footer } from './layout/Footer';
-
 export { default as Brand } from './ui/Brand';
 export { default as Navigation } from './ui/Navigation';
-
 export { default as NavigationItem } from './ui/core/NavigationItem';
-
 export { default as Hero } from './static/Hero';
+export { default as Authentication } from './layout/Authentication';
